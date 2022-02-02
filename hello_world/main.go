cab5b773844810ac8731180ff8a86d515985366a
RaminCH <ramin.chopurov@gmail.com>
2022-02-02 14:32:27 +0300

    added divide function

--- hello_world/main.go
+++ hello_world/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"net/http"
 )
@@ -23,20 +24,37 @@ func addValues(x, y int) int {
 	return x + y
 }
 
+func Divide(w http.ResponseWriter, r *http.Request) {
+	f, err := divideValues(100.0, 0.0)
+	if err != nil {
+		fmt.Fprintf(w, "cannot divide by 0")
+		return
+	}
+	fmt.Fprintf(w, fmt.Sprintf("%f divided by %f is %f", 100.0, 0.0, f))
+}
+
+func divideValues(x, y float32) (float32, error) {
+	if y <= 0 {
+		err := errors.New("cannot divide by zero")
+		return 0, err
+	}
+	result := x / y
+	return result, nil
+}
+
 func main() {
 
 	http.HandleFunc("/", Home)
 	http.HandleFunc("/about", About)
+	http.HandleFunc("/divide", Divide)
 
 	fmt.Println(fmt.Sprintf("Starting application on port: %s", portNumber))
 	_ = http.ListenAndServe(portNumber, nil)
 }
 
-
 //In terminal
 // ramie@ramie:~/go/src/Web_Part1/hello_world(master)$ go run main.go
 // Starting application on port: :8080
-
 
 //in browser
 // http://localhost:8080/
@@ -44,5 +62,9 @@ func main() {
 
 // http://localhost:8080/about
 // This is the about page and 2 + 2 is: 4
+
+
+// http://localhost:8080/divide
+// cannot divide by 0
 
 //Ctrl+C - cancel the process
