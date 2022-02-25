96c69282b32b80cd736043e1e0baa050ccb2aff8
RaminCH <ramin.chopurov@gmail.com>
2022-02-25 20:20:17 +0300

    27_reorganizing code, adding basic bootstrap style

--- hello_world/main.go
+++ hello_world/main.go
@@ -2,30 +2,10 @@ package main
 
 import (
 	"fmt"
-	"html/template"
 	"net/http"
 )
 
 const portNumber = ":8080"
-
-//Home is the home page handler
-func Home(w http.ResponseWriter, r *http.Request) {
-	renderTemplate(w, "home.page.tmpl")
-}
-
-//About is the about page handler
-func About(w http.ResponseWriter, r *http.Request) {
-	renderTemplate(w, "about.page.tmpl")
-}
-
-func renderTemplate(w http.ResponseWriter, tmpl string) {
-	parsedTemplate, _ := template.ParseFiles("./templates/" + tmpl)
-	err := parsedTemplate.Execute(w, nil)
-	if err != nil {
-		fmt.Println("error parsing template:", err)
-		return
-	}
-}
 
 func main() {
 
@@ -36,10 +16,8 @@ func main() {
 	_ = http.ListenAndServe(portNumber, nil)
 }
 
-
 // ramie@ramie:~/go/src/Web_Part1/hello_world(master)$ go run main.go
 // Starting application on port: :8080
-
 
 //In browser
 // http://localhost:8080/
